ec448c6f9eefec443fb214d051cdcdd27ca00236
B Marques <brunosmarques1995@hotmail.com>
2023-02-01 21:21:12 -0300

    Moved Entrypoint to Satoshi

--- Sandbox/src/lib/SandboxApp.cpp
+++ Sandbox/src/lib/SandboxApp.cpp
@@ -1,19 +1,2 @@
 #include "Satoshi.hpp"
-
-#ifdef ST_PLATFORM_WINDOWS
-
-#include <windows.h>
-
-INT APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine,
-	_In_ int nCmdShow)
-{
-	Satoshi::Console::Init();
-	Satoshi::Console::Log("Hello Logger");
-	Satoshi::Console::End();
-	while (true);
-	return 0;
-}
-
-#endif
+#include "Satoshi/Core/Entrypoint.hpp"
---
+++ Satoshi/src/include/Satoshi/Core/Entrypoint.hpp
@@ -0,0 +1,22 @@
+#ifndef ENTRYPOINT_HPP
+#define ENTRYPOINT_HPP
+
+#ifdef ST_PLATFORM_WINDOWS
+
+#include <windows.h>
+
+INT APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	Satoshi::Console::Init();
+	Satoshi::Console::Log("Hello Logger");
+	Satoshi::Console::End();
+	while (true);
+	return 0;
+}
+
+#endif
+
+#endif //ENTRYPOINT_HPP
