2e7d9f7066df2c8486c1b534f6668b9a6bf21d36
B Marques <brunosmarques1995@hotmail.com>
2023-02-06 00:13:28 -0300

    Gained main control

--- Sandbox/src/lib/SandboxApp.cpp
+++ Sandbox/src/lib/SandboxApp.cpp
@@ -2,5 +2,26 @@
 
 #include "MainApplication.hpp"
 
-IMPLEMENT_APP(MainApplication);
+int WINAPI wWinMain(HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance, 
+    LPWSTR lpCmdLine,
+    int nCmdShow)                             
+{
+    std::wstring cmdLine = lpCmdLine;
+    std::string s_cmdLine = std::string(cmdLine.begin(), cmdLine.end());
+    return wxEntry(hInstance, hPrevInstance, (char *)s_cmdLine.c_str(), nCmdShow);
+}
 
+//#define wxIMPLEMENT_APP_NO_MAIN(appname)                                    
+MainApplication& wxGetApp()
+{ 
+    return *static_cast<MainApplication*>(wxApp::GetInstance());
+}
+    
+wxAppConsole *wxCreateApp()                                             
+{                                                                       
+        wxAppConsole::CheckBuildOptions(WX_BUILD_OPTIONS_SIGNATURE, "your program");                    
+        return new MainApplication;
+}                                                                       
+
+wxAppInitializer wxTheAppInitializer((wxAppInitializerFunction)wxCreateApp);
